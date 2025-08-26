10dadc80fdb5d70e60f1a8796af30998e0456d62
Pawan Kumar Pandey <pawan4kp@gmail.com>
2025-08-26 20:22:08 +0530

    new way to solve rotate left

--- Arrays2/RotateLeft_by1.js
+++ Arrays2/RotateLeft_by1.js
@@ -7,4 +7,16 @@ for(let i = 0;i<=arr.length-2;i++){ // or use i < arr.length-1
 }
 arr[arr.length-1] = first
 
-process.stdout.write("After Rotation = "+arr)
+process.stdout.write("After Rotation = ")
+console.log(arr)
+
+// way 2 --> withought using extra space 
+arr = [1,2,3,4,5]
+
+for(let i=0;i<=arr.length-2; i++){
+    arr[i] = arr[i]^arr[i+1]
+    arr[i+1] = arr[i]^arr[i+1]
+    arr[i] = arr[i]^arr[i+1]
+}
+
+console.log(arr)
